344b443104b9809813f052221120c14644821a72
Jake Jackson <jake@gravitypdf.com>
2018-07-13 14:57:55 +1000

    Fix the plugin name on the licensing page

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -90,8 +90,36 @@ class Bootstrap extends Helper_Abstract_Addon {
 			]
 		);
 
+		$this->add_filters();
+
 		/* Run the setup */
 		parent::init( $classes );
+	}
+
+	/**
+	 * @since 0.1
+	 */
+	public function add_filters() {
+		add_filter( 'gfpdf_settings_licenses', [ $this, 'fix_plugin_short_name' ], 20 );
+	}
+
+	/**
+	 * Fix the plugin short name
+	 *
+	 * @param array $fields
+	 *
+	 * @return array
+	 *
+	 * @since 0.1
+	 */
+	public function fix_plugin_short_name( $fields ) {
+		foreach ( $fields as &$field ) {
+			if ( $field['id'] === 'license_' . $this->get_slug() ) {
+				$field['name'] = $this->get_name();
+			}
+		}
+
+		return $fields;
 	}
 
 	/**
